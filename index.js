e3d17612557f47a30a2fa48e0979c4ab26ff80d8
gergegdg reg eg <158379641+Idkmsmski@users.noreply.github.com>
2026-03-20 18:37:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -72,4 +72,7 @@ app.post("/follow", async (req, res) => {
 app.get("/", (req, res) => res.send("API is running."));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server live on port ${PORT}`));
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
